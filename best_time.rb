9c134c97f7c0c7c368b454ffe2b70536b899e0c6
Andrew Kane <andrew@getformidable.com>
2012-03-14 01:09:52 -0700

    No bar charts - we want continuous data

--- best_time.rb
+++ best_time.rb
@@ -2,36 +2,13 @@ class BestTime
   attr_reader :buckets
 
   TIERS = {
-    hour: {
-      range: 0...24,
-      bucket: lambda{|time| time.hour },
-      # graph options
-      label: lambda{|key| Time.new(2012, 1, 1, key).strftime("%l %P") },
-      step: 2
+    week: {
+      range: 0...168,
+      bucket: lambda{|time| time.wday*24 + time.hour },
     },
-    minute_of_day: {
-      range: 0...1440,
-      bucket: lambda{|time| (time.hour * 60) + time.min },
-      label: lambda{|key| Time.new(2012, 1, 1, key / 60, key % 60).strftime("%H:%M") },
-      step: 60
-    },
-    wday: {
-      range: 0...7,
-      bucket: lambda{|time| time.wday },
-      label: lambda{|key| Time.new(2012, 1, key + 1).strftime("%a") },
-      step: 1
-    },
-    month: {
-      range: 1..12,
-      bucket: lambda{|time| time.month },
-      label: lambda{|key| Time.new(2012, key, 1).strftime("%b") },
-      step: 1
-    },
-    minute: {
-      range: 0...60,
-      bucket: lambda{|time| time.min },
-      label: lambda{|key| Time.new(2012, 1, 1, 0, key).strftime("00:%M") },
-      step: 10
+    day: {
+      range: 0...96,
+      bucket: lambda{|time| (time.hour*60 + time.min)/15.0 }
     }
   }
 
@@ -47,7 +24,7 @@ class BestTime
     conversions.each do |conversion|
       mean = @tier[:bucket].call(conversion)
       size = range.max - range.min + 1
-      std = size / 12.0
+      std = size / 24
       #std = 1e-10
       value = 1 # could be weighted
       range.each do |key|
@@ -61,7 +38,7 @@ class BestTime
   end
 
   def graph
-    rows = buckets.to_a.map{|v| [@tier[:label].call(v[0]), ("%.2f" % v[1]).to_f] }
+    rows = buckets.to_a.map{|v| [v[0], ("%.2f" % v[1]).to_f] }
     str = File.read("chart.html").gsub("{{rows}}", rows.inspect)
     filename = "/tmp/chart.html"
     File.open(filename, "w"){|f| f.write(str) }
@@ -83,5 +60,5 @@ conversions = [
   now + 7200
 ]
 
-bt = BestTime.new(conversions, :hour)
+bt = BestTime.new(conversions, :week)
 bt.graph
